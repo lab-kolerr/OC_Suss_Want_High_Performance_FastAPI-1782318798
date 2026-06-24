bee51337cd1f8f0dc31c51f23d9e89de0158245a
lab-kolerr <lab.kolerr@kolerr.com>
2026-06-24 23:33:54 +0700

    Initial commit - src/server.rs

---
+++ src/server.rs
@@ -0,0 +1,7 @@
+use crate::config::Config;
+
+/// Starts the HTTP server.
+pub async fn start_http_server(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+    // Server setup logic here
+    Ok(())
+}
