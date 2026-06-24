3410282c2e8e314ff40cfdb8dd9551bbb25ae412
lab-kolerr <lab.kolerr@kolerr.com>
2026-06-24 23:33:44 +0700

    Initial commit - src/agent.rs

---
+++ src/agent.rs
@@ -0,0 +1,14 @@
+use sqlx::FromRow;
+use serde::{Deserialize, Serialize};
+use chrono::{DateTime, Utc};
+
+#[derive(Debug, FromRow, Serialize, Deserialize)]
+pub struct Agent {
+    pub id: i32,
+    pub swarm_id: i32,
+    pub name: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub is_deleted: bool,
+    pub deleted_at: Option<DateTime<Utc>>, 
+}
